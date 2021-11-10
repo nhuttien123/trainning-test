61f3285ad29e97b395c2276410a858c2f9718ebc
nhuttien2210 <tienlnps12361@fpt.edu.vn>
2021-11-10 18:01:24 +0700

    upload responsive banner

--- JS/slider.js
+++ JS/slider.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".banner-items").slick({
+  $(".banner__items").slick({
     prevArrow:
       "<button type='button' class='slick-button slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow:
@@ -7,8 +7,31 @@ $(document).ready(function () {
     dots: true,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 992,
       },
     ],
   });
+});
+
+const openToggle = document.getElementById("toggle");
+let isShow = false;
+
+openToggle.addEventListener("click", () => {
+  isShow = true;
+  if (isShow == true) {
+    document.getElementById("toggle-menu").style.left = "0";
+  } else {
+    document.getElementById("toggle-menu").style.left = "-100%";
+  }
+});
+
+const closeToggle = document.getElementById("close-toggle");
+
+closeToggle.addEventListener("click", () => {
+  isShow = false;
+  if (isShow == true) {
+    document.getElementById("toggle-menu").style.left = "0";
+  } else {
+    document.getElementById("toggle-menu").style.left = "-100%";
+  }
 });
