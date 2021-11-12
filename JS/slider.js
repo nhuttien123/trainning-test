cd0c104ec314f902d77d2dda9cf689b8f6f694a2
nhuttien2210 <tienlnps12361@fpt.edu.vn>
2021-11-12 16:36:46 +0700

    add main home_site

--- JS/slider.js
+++ JS/slider.js
@@ -1,10 +1,25 @@
 $(document).ready(function () {
-  $(".banner__items").slick({
+  $(".banner-items").slick({
     prevArrow:
       "<button type='button' class='slick-button slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow:
       "<button type='button' class='slick-button slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
     dots: true,
+    responsive: [
+      {
+        breakpoint: 992,
+      },
+    ],
+  });
+});
+
+$(document).ready(function () {
+  $(".section-service-slide").slick({
+    prevArrow:
+      "<button type='button' class='slick-button slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-button slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    dots: false,
     responsive: [
       {
         breakpoint: 992,
