f8858ff6c5075e23deace547df950ff0fffc9616
nhuttien123 <89700239+nhuttien123@users.noreply.github.com>
2021-11-13 22:37:52 -0800

    add home_site

--- JS/slider.js
+++ JS/slider.js
@@ -22,7 +22,52 @@ $(document).ready(function () {
     dots: false,
     responsive: [
       {
+        breakpoint: 991,
+        settings: {
+          dots: true,
+        },
+      },
+    ],
+  });
+});
+
+$(document).ready(function () {
+  $(".gallery-slide").slick({
+    prevArrow:
+      "<button type='button' class='slick-button slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-button slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    swipeToSlide: true,
+    centerMode: true,
+    centerPadding: "5px",
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          centerMode: false,
+        },
+      },
+      {
         breakpoint: 992,
+        settings: {
+          dots: true,
+          centerMode: false,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          dots: true,
+          centerMode: false,
+          slidesToShow: 1,
+        },
       },
     ],
   });
